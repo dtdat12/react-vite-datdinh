31ff90f26e4d174744a10ba01a1cf07621a78a2a
unknown <datdinh>
2024-06-22 13:02:03 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
+  // function myFunction() {
+  //   console.log("My Function");
+  // }
+  const myFunction = () => {
+    console.log("My Function");
+  }
+  myFunction();
   return (
     <>
       <div>

08c7907e4fa95375963ba2d90cecf50cfe5b7093
unknown <datdinh>
2024-06-22 15:43:49 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,15 @@
+//JSX
+//Fragment
+import './style.css';
+
 const MyComponent = () => {
     return (
+        <>
       <div>Helooo</div>
+      <div className="datdinh"
+            style={{ borderRadius: "10px"}}
+        >datdinh</div>
+      </>
     );
   }
 
